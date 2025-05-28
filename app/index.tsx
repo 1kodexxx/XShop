d1c8097ef1dd03ed3165c4a50fe83bda4d19ebc2
pvntheraxxx <pvntheraxxx@gmail.com>
2025-05-28 18:46:37 +0300

    new started

--- app/index.tsx
+++ app/index.tsx
@@ -1,33 +1,18 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { Link } from "expo-router";
 
-type Props = {};
-
-const WelcomeScreen = (props: Props) => {
+const index = () => {
   return (
-    <View style={styles.container}>
-      <Text>Welcome Screen</Text>
-      <Link href={"/signin"} asChild>
-        <TouchableOpacity>
-          <Text>Go to SignIn Screen</Text>
-        </TouchableOpacity>
-      </Link>
-      <Link href={"/signup"} asChild>
-        <TouchableOpacity>
-          <Text>Go to SignUp Screen</Text>
-        </TouchableOpacity>
-      </Link>
+    <View style={styles.backround}>
+      <Text>index</Text>
     </View>
   );
 };
 
-export default WelcomeScreen;
+export default index;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  backround: {
+    backgroundColor: "red",
   },
 });
